3b6be5b31d39c66a82b3fec18c5b71378f4055f9
WIT\macphaild <macphaild@wit.edu>
2019-07-19 14:58:14 -0400

    Hiding API key

--- app/src/main/java/com/example/explorationapp/MapsActivity.kt
+++ app/src/main/java/com/example/explorationapp/MapsActivity.kt
@@ -1,18 +1,22 @@
 package com.example.explorationapp
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-
-    private lateinit var mMap: GoogleMap
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     /**
@@ -33,11 +41,43 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // TODO: allow users to save locations, create functions to use locations
+        val savedLocation = LatLng(42.36, -71.06)
+        map.addMarker(MarkerOptions().position(savedLocation).title("User checkpoint!"))
+        // 0 = zoomed far out
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(savedLocation, 12.0f))
+        map.moveCamera(CameraUpdateFactory.newLatLng(savedLocation))
+        map.uiSettings.isZoomControlsEnabled = true
+        // sets MapsActivity as the callback when a user clicks a marker on the map
+        // TODO: Implement function to handle map markers
+        map.setOnMarkerClickListener(this)
+
+        setUpMap();
     }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
+    /**
+     * check if app has been granted ACCESS_FINE_LOCATION, if it hasn't then request it.
+     */
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE
+            )
+            return
+        }
+    }
+
 }
